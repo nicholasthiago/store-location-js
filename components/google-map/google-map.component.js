c5f6409d77ce3c0dd81a4a4a1161ff5a486c8774
Nicholas Thiago <nicholasxavis23@gmail.com>
2021-03-30 16:41:38 -0300

    project : end

--- components/google-map/google-map.component.js
+++ components/google-map/google-map.component.js
@@ -1,41 +1,41 @@
-// ------------------------------------- //
-// --- Munddi App - Google Map --------- //
+// -------------------------------------- //
+// --- Munddi App - Google Map ---------- //
 
 // NE: 'Lat: -21.7996 Lon: -44.8562'
 // SW: 'Lat: -24.5814 Lon: -49.1860'
 
+let marker;
 let storeList  = [];
-let markerList = [];
-let urlRequest = 'https://munddi.com/dev/pdvs?ne_lat=-21.79&ne_lng=-44.85&sw_lat=-24.58&sw_lng=-49.18';
+let urlRequest = 'https://munddi.com/dev/pdvs?ne_lat=10&ne_lng=-30&sw_lat=-30&sw_lng=-70';
 
+// --- Leaflet Map Render Function ------ //
+var map = L.map(document.querySelector('section.app-google-map'), {
+	center  	: [ -23.185, -44.821 ],
+    zoom		: 8 ,
+});
+
+// --- Leaflet Tile Layer Add ----------- //
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar'}).addTo(map);
+
+// --- Munddi Stores Request ------------ //
 fetch(urlRequest)
 	.then((resp) => resp.json())
 	.then(function(data) {
-		storeList = data.data;
-		console.log( data.data )
+		let r = data.data;
+		for (let i=0; i < r.length; i++) {
+			marker = new L.marker([
+				r[i].lat,
+				r[i].lng
+			]).addTo(map)
+			.on('mousedown',onClick);
+		};
 	})
 	.catch(function(error) { console.log(error) });
 
-function initMap() {
-	const center = new google.maps.LatLng( -23.185, -47.021 );
-	const map = new google.maps.Map(document.querySelector('section.app-google-map'), {
-		zoom	  : 8		 	,
-		center	  : center		,
-		mapTypeId : 'roadmap'	,
-	});
+// --- Marker Click Function ------------ //
+function onClick (e) {
+	let data = e.latlng;
+	let url = `https://www.google.com/maps/search/?api=1&query=${data.lat},${data.lng}`;
 
-	const marker = new google.maps.Marker({
-		position: center,
-		map: map,
-	});
-
-	return storeList.map( (s,i) => {
-		markerList[i] = new google.maps.Marker({
-			map: map,
-			position: {
-				lng: Number(s.lng),
-				lat: Number(s.lat),
-			},
-		});
-	});
+	return window.location = url;
 };
